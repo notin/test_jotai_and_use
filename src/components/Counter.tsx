e2da8f4fbdcdcdb51e8af69cb2aec229884960cb
notin <chadvansyoc11@gmail.com>
2023-03-06 14:08:57 -0800

    working with jotai

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,6 +2,8 @@ import  {atom, useAtom} from "jotai";
 import React from "react";
 import {countAtom} from "../App";
 
+import { experimental_use as use, Suspense, useState, useEffect } from "react";
+
 const Counter = () => {
     const [count, setCount] = useAtom(countAtom);
     return (

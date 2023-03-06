6028598d0d86bc2744898485b3760bda1150eef4
notin <chadvansyoc11@gmail.com>
2023-03-06 13:21:21 -0800

    adfsfs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: test_jotai_and_use</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));

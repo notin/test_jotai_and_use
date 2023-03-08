c120e1c0205472a1edcc9fda9817b3101aca0744
notin <chadvansyoc11@gmail.com>
2023-03-07 18:18:49 -0800

    working with jotai

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+// @ts-ignore
 import { experimental_use as use, Suspense, useState, useEffect } from "react";
 import Names from "./components/Names";
 import useCachedFetch from "./hooks/useCachedFetch";
 
 function App() {
   const [initialized, setInitialized] = useState(false);
+  // @ts-ignore
   async function init() {
     const ids = await useCachedFetch("/ids.json");
+    // @ts-ignore
     await Promise.all(ids.data.map((id) => useCachedFetch(`/${id}.json`)));
     setInitialized(true);
   }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,18 @@
+import  {atom, useAtom} from "jotai";
+import React from "react";
+import {countAtom} from "../App";
+
+//@ts-ignore
+import { experimental_use as use, Suspense, useState, useEffect } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useAtom(countAtom);
+    return (
+        <div>
+            <div>Count: {count}</div>
+            <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+            <button onClick={()=> setCount((c) => c - 1)}>Decrement</button>
+        </div>
+    );
+};
+export default Counter;
